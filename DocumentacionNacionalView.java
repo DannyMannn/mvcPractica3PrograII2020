848f47b502e79d6d2022f7b332589975b23e38e5
Danny <65949272+DannyMannn@users.noreply.github.com>
2020-11-27 01:39:44 -0600

    Reusable GUI JPanel Component

--- DocumentacionNacionalView.java
+++ DocumentacionNacionalView.java
@@ -1,13 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
-public class DocumentacionNacionalView {
+public class DocumentacionNacionalView extends JPanel{
+	private JLabel curpLabel = new JLabel("Ingresa CURP: ");
+	private JTextField curpField = new JTextField(20);
+
 	public String curpEntrada;
 
-	public DocumentacionNacionalView() throws Exception {
-		Scanner scanner = new Scanner(System.in);
+	public DocumentacionNacionalView() {
+		this.setLayout(new GridBagLayout());
 
-		System.out.print("Ingresa CURP: ");
-		this.curpEntrada = scanner.nextLine();
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets = new Insets(10, 10, 10, 10);
+
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		this.add(curpLabel,constraints);
+
+		constraints.gridx = 1;
+		this.add(curpField,constraints);
+
+		this.setBorder(BorderFactory.createTitledBorder(
+				BorderFactory.createEtchedBorder(), "DocumentacionNacionalView"));
 	}
 }
-
