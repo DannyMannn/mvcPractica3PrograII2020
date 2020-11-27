e799603343b66f69c524000dd66e3f350c381788
Danny <65949272+DannyMannn@users.noreply.github.com>
2020-11-27 09:38:18 -0600

    Update MaletaView.java

--- MaletaView.java
+++ MaletaView.java
@@ -1,17 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class MaletaView {
-	public double pesoMaletaEntrada;
+public class MaletaView extends JPanel{
+	private JLabel pesoMaletaLabel = new JLabel("Ingrese Peso");
+	private JTextField pesoMaletaTextField = new JTextField(20);
 
-	public MaletaView() throws Exception {
-		Scanner scanner = new Scanner(System.in);
+	private double pesoMaletaEntrada = 0;
 
-		try {
-			System.out.println("Ingrese el peso de la maleta: ");
-			this.pesoMaletaEntrada = scanner.nextDouble();
-			scanner.nextLine();
-		} catch (Exception e) {
-			throw new Exception("¡Error en la entrada de datos!");
-		}
+	public double getPesoMaletaEntrada(){
+		return pesoMaletaEntrada;
+	}
+	MaletaView(){
+		this.setLayout(new GridBagLayout());
+
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets = new Insets(10, 10, 10, 10);
+
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		this.add(pesoMaletaLabel,constraints);
+
+		constraints.gridx = 1;
+		this.add(pesoMaletaTextField,constraints);
+
+		this.setBorder(BorderFactory.createTitledBorder(
+				BorderFactory.createEtchedBorder(), "Registrar Maleta"));
 	}
 }
