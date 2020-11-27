bab49c2a7b23b9a3ed0b55fbb4acf9e91fd94f4a
Danny <65949272+DannyMannn@users.noreply.github.com>
2020-11-27 01:38:17 -0600

    Reusable GUI JPanel component

--- DocumentacionInternacionalView.java
+++ DocumentacionInternacionalView.java
@@ -1,27 +1,64 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.util.Scanner;
 
-public class DocumentacionInternacionalView {
+public class DocumentacionInternacionalView extends JPanel {
+	private JLabel numPasaporteLabel = new JLabel("Número de Pasaporte: ");
+	private JLabel tipoVisaLabel = new JLabel("Tipo de Visa: ");
+	private JLabel vigenciaLabel = new JLabel("Vigencia: ");
+	private JTextField numPasaporteTextField = new JTextField(20);
+	private JComboBox<String> tipoVisaComboBox = new JComboBox<>();
+	private JTextField vigenciaTextField = new JTextField(20);
+
 	public int numPasaporteEntrada;
 	public EnumVisa visaEntrada;
 	public int vigenciaEntrada;
 
-	public DocumentacionInternacionalView() throws Exception {
-		Scanner scanner = new Scanner(System.in);
+	public DocumentacionInternacionalView(){
+		this.setLayout(new GridBagLayout());
 
-		try {
-			System.out.println("Ingresa numero de pasaporte: ");
-			this.numPasaporteEntrada = scanner.nextInt();
-			scanner.nextLine();
+		tipoVisaComboBox.setModel(new DefaultComboBoxModel<>(new String[] { "TURISTA", "PRIMERA_CLASE", "EJECUTIVO" }));
 
-			System.out.println("Ingresa tipo de visa (TURISTA, TRABAJO, ATENCION_MEDICA): ");
-			this.visaEntrada = EnumVisa.valueOf(scanner.nextLine().toUpperCase());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets = new Insets(10, 10, 10, 10);
 
-			System.out.println("Años de vigencia de la visa: ");
-			this.vigenciaEntrada = scanner.nextInt();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		this.add(numPasaporteLabel,constraints);
 
-		} catch (Exception e) {
-			throw new Exception("¡Error en la entrada de datos!");
-		}
+		constraints.gridx = 1;
+		this.add(numPasaporteTextField,constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		this.add(tipoVisaLabel,constraints);
+
+		constraints.gridx = 1;
+		this.add(tipoVisaComboBox,constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		this.add(vigenciaLabel,constraints);
+
+		constraints.gridx = 1;
+		this.add(vigenciaTextField,constraints);
+
+		this.setBorder(BorderFactory.createTitledBorder(
+				BorderFactory.createEtchedBorder(), "DocumentacionInternacionalView"));
 	}
+
+	public static void main(String[] args) {
+		JFrame mainFrame = new JFrame();
+		mainFrame.setLayout(new BorderLayout());
+		DocumentacionInternacionalView view = new DocumentacionInternacionalView();
+		DocumentacionNacionalView view2 = new DocumentacionNacionalView();
+		mainFrame.getContentPane().add(view,BorderLayout.WEST);
+		mainFrame.getContentPane().add(view2,BorderLayout.EAST);
+		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+		mainFrame.pack();
+	}
+
 }
 
